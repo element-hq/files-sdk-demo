f0b31ff8fd6391d57b5d94c99ada009eada8d183
Hugh Nimmo-Smith <hughns@element.io>
2022-07-25 13:12:19 +0100

    fix: don't request offline_access token

--- src/ClientManager.ts
+++ src/ClientManager.ts
@@ -299,7 +299,7 @@ export class ClientManager {
                 client_secret: this.oidcClientSecret,
                 redirect_uri: this.redirect_uri,
                 accessTokenExpiringNotificationTimeInSeconds: 30,
-                scope: `openid offline_access urn:matrix:api:* urn:matrix:device:${this.deviceId}`,
+                scope: `openid urn:matrix:api:* urn:matrix:device:${this.deviceId}`,
             });
             this.userManager.events.addUserLoaded(({ access_token, expires_in }) => {
                 log.debug(`Access token renewed with new expiry in ${expires_in}s`);
